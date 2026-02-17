f4ecb9639943cba7daf5afb4196152112159d4a8
andreasbloch <35136085+andreasbloch@users.noreply.github.com>
2026-02-17 19:03:45 +0100

    Delete rss-widget.js

--- rss-widget.js
+++
@@ -1,8 +0,0 @@
-class HeaderTextWidget extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `<span style="color: white; font-weight: bold; padding: 0 10px;">
-                        Dein Text hier
-                      </span>`;
-  }
-}
-customElements.define('header-text-widget', HeaderTextWidget);
